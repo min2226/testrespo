91565eeb6361acd26d6b1f36551dd1f13dfe61a8
s-onair .co <s-onair@Nam-ui-MacBook-Air.local>
2014-06-13 18:49:52 +0900

    asdfasdf

--- testrespo/SNMasterViewController.m
+++ testrespo/SNMasterViewController.m
@@ -27,7 +27,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     self.navigationItem.leftBarButtonItem = self.editButtonItem;
-    int aaadddasdfasdf;
+    int a;
     UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
     self.navigationItem.rightBarButtonItem = addButton;
 }
