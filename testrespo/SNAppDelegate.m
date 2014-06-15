d9057a65c2c3438df49ca9a3e8bc40722236ce49
s-onair .co <s-onair@Nam-ui-MacBook-Air.local>
2014-06-15 19:57:52 +0900

    asdf

--- testrespo/SNAppDelegate.m
+++ testrespo/SNAppDelegate.m
@@ -13,6 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    // it is changed from xcode
     return YES;
 }
 							
